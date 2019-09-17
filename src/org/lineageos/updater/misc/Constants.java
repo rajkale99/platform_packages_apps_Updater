93a64ec68aad6238a2e67a87f280b3f3f2f3bb66
Chau Truong Thinh <chautruongthinh@gmail.com>
2019-09-17 16:58:58 +0700

    Updater: update server URL & changelog URL
    * Update Updater for new changelog URL

--- src/org/lineageos/updater/misc/Constants.java
+++ src/org/lineageos/updater/misc/Constants.java
@@ -45,6 +45,7 @@ public final class Constants {
     public static final String PROP_RELEASE_TYPE = "ro.superior.releasetype";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "superior.updater.allow_downgrading";
     public static final String PROP_UPDATER_URI = "superior.updater.uri";
+    public static final String PROP_VERSION = "ro.superior.version";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
