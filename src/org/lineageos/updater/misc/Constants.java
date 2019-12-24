354aa462c86b29a5a24928dbaa5e3b3e7d04de92
rajkale99 <kaleraj.rk@gmail.com>
2019-12-24 19:57:49 +0530

    Refractor Legionos

--- src/org/lineageos/updater/misc/Constants.java
+++ src/org/lineageos/updater/misc/Constants.java
@@ -20,9 +20,9 @@ public final class Constants {
     private Constants() {
     }
 
-    public static final String SF_PROJECT_NAME = "arrow-os";
+    public static final String SF_PROJECT_NAME = "legionrom";
     // version will be set according to the update version in the mirrors function
-    public static final String SF_PROJECT_ROOT_PATH_NAME = "arrow-";
+    public static final String SF_PROJECT_ROOT_PATH_NAME = "legion-";
 
     public static final String AB_PAYLOAD_BIN_PATH = "payload.bin";
     public static final String AB_PAYLOAD_PROPERTIES_PATH = "payload_properties.txt";
@@ -46,12 +46,13 @@ public final class Constants {
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
     public static final String PROP_BUILD_VERSION = "ro.modversion";
     //public static final String PROP_BUILD_VERSION_INCREMENTAL = "ro.build.version.incremental";
-    public static final String PROP_DEVICE = "ro.arrow.device";
+
+    public static final String PROP_DEVICE = "ro.legion.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
-    public static final String PROP_RELEASE_TYPE = "ro.arrow.releasetype";
-    public static final String PROP_ZIP_TYPE = "ro.arrow.ziptype";
+    public static final String PROP_RELEASE_TYPE = "ro.legion.releasetype";
+    public static final String PROP_ZIP_TYPE = "ro.legion.ziptype";
     public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading";
-    public static final String PROP_UPDATER_URI = "arrow.updater.uri";
+    public static final String PROP_UPDATER_URI = "legion.updater.uri";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
