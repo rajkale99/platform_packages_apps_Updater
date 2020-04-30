582691861c601f6542158724140545ceea7c15a7
Rajkale99 <kaleraj.rk@gmail.com>
2020-04-30 16:25:17 +0000

    Refractor ColtOS Signed-off-by: Raj Kale <kaleraj.rk@gmail.com

--- src/org/lineageos/updater/misc/Constants.java
+++ src/org/lineageos/updater/misc/Constants.java
@@ -40,15 +40,14 @@ public final class Constants {
     public static final String PROP_AB_DEVICE = "ro.build.ab_update";
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
     public static final String PROP_BUILD_VERSION = "ro.modversion";
-    public static final String PROP_UPDATER_URI = "legion.updater.uri";
-    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "legion.updater.allow_downgrading";
-    public static final String PROP_VERSION = "ro.legionversion";
+    public static final String PROP_UPDATER_URI = "colt.updater.uri";
+    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "colt.updater.allow_downgrading";
+    public static final String PROP_VERSION = "ro.coltversion";
     //public static final String PROP_BUILD_VERSION_INCREMENTAL = "ro.build.version.incremental";
 
-    public static final String PROP_DEVICE = "ro.legion.device";
+    public static final String PROP_DEVICE = "ro.colt.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
-    public static final String PROP_RELEASE_TYPE = "ro.legion.releasetype";
-    public static final String PROP_ZIP_TYPE = "ro.legion.ziptype";
+    public static final String PROP_RELEASE_TYPE = "ro.colt.releasetype";
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
     public static final String PREF_INSTALL_PACKAGE_PATH = "install_package_path";
